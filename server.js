6fafad5c283f3cbd6f2ef6778e57161f04477352
rajaprerak <rajaprerak@gmail.com>
2021-03-01 18:35:44 -0700

    multiple files upload

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const multer = require('multer');
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require('multer');
+var app = express();
 const fs = require('fs');
+
 const AWS = require('aws-sdk');
 
 const BUCKET_NAME = 'cc-project-input-images';
@@ -13,7 +16,7 @@ const s3 = new AWS.S3({
 const uploadFile = (fileName) => {
 
     const path = require("path");
-    const fileContent = fs.readFileSync(path.resolve(__dirname, "./uploads/images/"+fileName));
+    const fileContent = fs.readFileSync(path.resolve(__dirname, "./uploads/"+fileName));
 
     const params = {
         Bucket: BUCKET_NAME,
@@ -29,29 +32,39 @@ const uploadFile = (fileName) => {
     });
 };
 
+app.use(bodyParser.json());
+
+
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname) 
-    }
-  })
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) 
+  }
+})
 
-var upload = multer({ storage: storage });
-const app = express();
-const PORT = 3000;
-
-app.use(express.static('public'));
-
-app.post('/upload_image', upload.single('photo'), (req, res) => {
-    if(req.file) {
-        uploadFile(req.file.filename)
-        res.json('success');
-    }
-    else throw 'error';
+var upload = multer({ storage: storage }).array('userPhoto',1000);
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(PORT, () => {
-    console.log('Listening at ' + PORT );
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+
+        if(err) {
+          console.log(err)
+            return res.end("Error uploading file.");
+        }
+        for (const index in req.files) {  
+          console.log(`${req.files[index].filename} is at position ${index}`)
+          uploadFile(req.files[index].filename)
+        }
+        res.end("File is uploaded");
+    });
+
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
 });

412cd851619158e02cff4ae62dc9b75f16d861e8
varadbhogayata <vbhogayata@gmail.com>
2021-03-06 14:39:42 -0700

    add results on client side (working)

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require("express");
+
 var bodyParser = require("body-parser");
 var multer = require('multer');
 var app = express();
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
+
+// app.set('view engine', 'html');
+
 const fs = require('fs');
 const AWS = require('aws-sdk');
 const BUCKET_NAME = 'cc-project-input-images';
@@ -13,6 +20,16 @@ const s3 = new AWS.S3({
     accessKeyId: 'AKIA5NQGXQ7RWW26VIB4',
     secretAccessKey: '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'
 });
+
+
+var receiveParams = {
+  AttributeNames: ["SentTimestamp"],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: ["All"],
+  QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
+  VisibilityTimeout: 1,
+  WaitTimeSeconds: 0
+  };
 
 const uploadFile = (fileName) => {
 
@@ -68,165 +85,70 @@ app.post('/api/photo',function(req,res){
 
 });
 
-var NumOfMessages = 0;
+var NumOfMessages = 10;
+var queParams = {
+  QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
+  AttributeNames : ['All'],
+};
 
+var dataDict = {}
 app.get('/receive', function(req, res){
   
-  var dataDict = {}
-  while (true){
-    
-    var queParams = {
-      QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
-      AttributeNames : ['All'],
-    };
-    sqs.getQueueAttributes(queParams, function(err, data){
-      console.log("Inside sqs.getQueueATtributes")
-      if (err) {
-             console.log("Error", err);
-           } else {
-             
-            NumOfMessages = parseInt(data['Attributes']['ApproximateNumberOfMessages']);
-            console.log(data);          
-            console.log(data['Attributes']['ApproximateNumberOfMessages']);
-            console.log("Num of msg after get Queue Attributes: "+NumOfMessages)
-          }              
-    });
-    
-    if (NumOfMessages === 0){
-      break;
-    
-    }
-     receiveMessage()
+  receiveMessage();
+  for (var key of Object.keys(dataDict)) {
+    console.log(key + " -> " + dataDict[key])
   }
+  console.log("message: "+dataDict);
+  // dataDict['sample'] = 'sample';
+  res.render("index", {dataDict:dataDict});
+  // res.json(dataDict);
+
 });
-    
-    // store data in dict and return dictionary
-    // console.log("Num of MESSAGES: "+NumOfMessages);
   
-  
+var receiveMessage = function() {
+  sqs.receiveMessage(receiveParams, function(err, data) {
+      if(err){
+          console.log(err);
+          }
+      if (data.Messages) {
+          console.log("inside data.messages");
+          for (var i = 0; i < data.Messages.length; i++) {
+            console.log("--------------------  inside For Loop --------------------" +i);
+              var message = data.Messages[i];              
+              // execute logic
+              NumOfMessages = data.Messages.length;
+              // console.log("Number of messages received: "+NumOfMessages);
+              // console.log("Received message: "+JSON.stringify(data.Messages[i]));
+              // console.log("Message body: "+data.Messages[i].Body);                      
+              const recvDataStr = JSON.stringify(data.Messages[i]);
+              const recvData = data.Messages[i];
+              console.log("Data Received: "+recvData['MessageAttributes']['output']['StringValue']);
+              result = recvData['MessageAttributes']['output']['StringValue'].split("#");
+              imageName = result[0];
+              ans = result[1];
+              console.log("Image: "+imageName);
+              console.log("Ans: "+ans);
+              dataDict[imageName] = ans;
+              removeFromQueue(message);
+          }
+          receiveMessage();
+      } else {
+          setTimeout(function() {
+              receiveMessage()
+          }, 10 * 1000);
 
-
-// function getQueueAttributes() {
-
-//   console.log("Inside getQueueATtributes")
-//   var queParams = {
-//     QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
-//     AttributeNames : ['All'],
-//   };
-//   sqs.getQueueAttributes(queParams, function(err, data){
-
-//     console.log("Inside sqs.getQueueATtributes")
-//     if (err) {
-//            console.log("Error", err);
-//          } else {
-           
-//           NumOfMessages = parseInt(data['Attributes']['ApproximateNumberOfMessages']);
-//           console.log("INSIDE SQS.GET NumOfMES: "+NumOfMessages);
-//           console.log(data);          
-//           console.log(data['Attributes']['ApproximateNumberOfMessages']);
-          
-//         }              
-//   });
-//   // console.log("Inside getQueueATtributes --> NumOfMessages: "+NumOfMessages);
-//   return NumOfMessages;
-// }
-
-const getQueueAttributes = () => {
-  
-}
-
-
-function receiveMessage(){
-  var params = {
-    AttributeNames: ["SentTimestamp"],
-    MaxNumberOfMessages: 1,
-    MessageAttributeNames: ["All"],
-    QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
-    VisibilityTimeout: 1,
-    WaitTimeSeconds: 0
-   };
-
-   sqs.receiveMessage(params, function(err, data) {
-    if (err) {
-      console.log("Receive Error in web tier", err);
-    } else if (data.Messages) {
-      console.log("Number of messages received: "+data.Messages.length);
-      console.log("Received message: "+JSON.stringify(data.Messages[0]));
-      console.log("Message body: "+data.Messages[0].Body);
-
-      // const recvData = JSON.parse(data.Messages[0].Body);
-      const recvDataStr = JSON.stringify(data.Messages[0]);
-      const recvData = data.Messages[0];
-      console.log("Data Received: "+recvData['MessageAttributes']['output']['StringValue']);
-      result = recvData['MessageAttributes']['output']['StringValue'].split("#");
-      // imageName = result.split(".")[0];
-      // ans = result[1];
-      console.log("Image: "+result[0]);
-      console.log("Ans: "+result[1]);
-
-      var deleteParams = {
-        QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
-        ReceiptHandle: data.Messages[0].ReceiptHandle
-      };
-      
-      sqs.deleteMessage(deleteParams, function(err, data) {
-        if (err) {
-          console.log("Delete Error", err);
-        } else {
-          console.log("Message Deleted", data);
-        }
-      });
-    } else {
-      console.log("No messages received!");
-    }
+      }
   });
-}
-// const receiveMessage = () => {
-//   var params = {
-//     AttributeNames: ["SentTimestamp"],
-//     MaxNumberOfMessages: 1,
-//     MessageAttributeNames: ["All"],
-//     QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
-//     VisibilityTimeout: 1,
-//     WaitTimeSeconds: 0
-//    };
+};
 
-//    sqs.receiveMessage(params, function(err, data) {
-//     if (err) {
-//       console.log("Receive Error in web tier", err);
-//     } else if (data.Messages) {
-//       console.log("Number of messages received: "+data.Messages.length);
-//       console.log("Received message: "+JSON.stringify(data.Messages[0]));
-//       console.log("Message body: "+data.Messages[0].Body);
-
-//       // const recvData = JSON.parse(data.Messages[0].Body);
-//       const recvDataStr = JSON.stringify(data.Messages[0]);
-//       const recvData = data.Messages[0];
-//       console.log("Data Received: "+recvData['MessageAttributes']['output']['StringValue']);
-//       result = recvData['MessageAttributes']['output']['StringValue'].split("#");
-//       // imageName = result.split(".")[0];
-//       // ans = result[1];
-//       console.log("Image: "+result[0]);
-//       console.log("Ans: "+result[1]);
-
-//       var deleteParams = {
-//         QueueUrl: "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
-//         ReceiptHandle: data.Messages[0].ReceiptHandle
-//       };
-      
-//       sqs.deleteMessage(deleteParams, function(err, data) {
-//         if (err) {
-//           console.log("Delete Error", err);
-//         } else {
-//           console.log("Message Deleted", data);
-//         }
-//       });
-//     } else {
-//       console.log("No messages received!");
-//     }
-//   });
-  
-// }
+var removeFromQueue = function(message) {
+  sqs.deleteMessage({
+      QueueUrl : "https://sqs.us-east-1.amazonaws.com/922358351843/cc-project1-sqs-response",
+      ReceiptHandle : message.ReceiptHandle
+  }, function(err, data) {
+      err && console.log(err);
+  });
+};
 
 const sendMessage = (url) => {
     var params = {
